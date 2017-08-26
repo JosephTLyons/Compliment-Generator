a89d57e3d2a1d12a08822d046701afd51bbb005a
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2017-08-26 06:09:44 -0400

    formatting

--- Compliment Generator.py
+++ Compliment Generator.py
@@ -19,7 +19,8 @@ adjectives = "sexy", "beautiful", "astonishing", "fabulous", "extraordinary", "g
              "courageous", "perfect", "honest", "charismatic", "breathtaking", "classy", "affectionate", "compassionate", \
              "resourceful", "swag-tastic", "cooperative", "exuberant", "productive", "stellar", "divine", "adventurous", \
              "fearless", "gregarious", "rational" , "sincere" , "witty", "versatile", "sociable", "pro-active", "practical", \
-             "optimistic", "loyal", "imaginative", "humorous", "fearless", "dynamic", "determined", "diligent", "courteous", "reasonable"
+             "optimistic", "loyal", "imaginative", "humorous", "fearless", "dynamic", "determined", "diligent", "courteous", \
+             "reasonable"
 nounsTwo   = "duck", "girl", "woman", "princess", "queen", "daisy", "invdividual", "intellect", "tulip", "koala bear", \
              "sex symbol", "specimen", "mermaid", "watermelon", "goose", "being", "goddess", "savant", "companion", \
              "baby-maker", "heart-throb", "citizen", "genius", "baller", "badass", "marsupial", "unicorn"
