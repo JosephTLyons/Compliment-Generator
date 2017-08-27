cac0970dd076120077bd62b1294f521f8fdff49f
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2017-08-27 09:17:00 -0400

    Added a few words

--- Words.py
+++ Words.py
@@ -1,5 +1,6 @@
 # Words
-nouns      = "Baby", "Honey", "Love", "Sweety", "Pumpkin", "Boo-Boo", "Bae", "Girl", "Dearest", "Bbgrl", "Sugar"
+nouns      = "Baby", "Honey", "Love", "Sweety", "Pumpkin", "Boo-Boo", "Bae", "Girl", "Dearest", "Bbgrl", "Sugar", \
+             "Apple Dumpling"
 
 adverbs    = "super", "extremely", "very", "especially", "really", "perfectly", "completely", "entirely", "incredibly"
 
@@ -9,9 +10,9 @@ adjectives = "sexy", "beautiful", "astonishing", "fabulous", "extraordinary", "g
              "resourceful", "swag-tastic", "cooperative", "exuberant", "productive", "stellar", "divine", "adventurous", \
              "fearless", "gregarious", "rational" , "sincere" , "witty", "versatile", "sociable", "pro-active", "practical", \
              "optimistic", "loyal", "imaginative", "humorous", "fearless", "dynamic", "determined", "diligent", "courteous", \
-             "reasonable"
+             "reasonable",
 
 nounsTwo   = "duck", "girl", "woman", "princess", "queen", "daisy", "invdividual", "intellect", "tulip", "koala bear", \
              "sex symbol", "specimen", "mermaid", "watermelon", "goose", "being", "goddess", "savant", "companion", \
              "baby-maker", "heart-throb", "citizen", "genius", "baller", "badass", "marsupial", "unicorn", "hero", "saint", \
-             "legend", "god"
+             "legend", "god", "godess"
