0abd494ef84b0f76e6afd12b0ae225f49405a1f4
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2017-08-27 08:52:40 -0400

    Formatted to be more readable

--- Words.py
+++ Words.py
@@ -1,6 +1,8 @@
 # Words
 nouns      = "Baby", "Honey", "Love", "Sweety", "Pumpkin", "Boo-Boo", "Bae", "Girl", "Dearest", "Bbgrl", "Sugar"
+
 adverbs    = "super", "extremely", "very", "especially", "really", "perfectly", "completely", "entirely", "incredibly"
+
 adjectives = "sexy", "beautiful", "astonishing", "fabulous", "extraordinary", "gorgeous", "independent", "glowing", \
              "responsible", "passionate", "kind", "caring", "impressive", "marvelous", "stupendous", "confident", \
              "courageous", "perfect", "honest", "charismatic", "breathtaking", "classy", "affectionate", "compassionate", \
@@ -8,6 +10,7 @@ adjectives = "sexy", "beautiful", "astonishing", "fabulous", "extraordinary", "g
              "fearless", "gregarious", "rational" , "sincere" , "witty", "versatile", "sociable", "pro-active", "practical", \
              "optimistic", "loyal", "imaginative", "humorous", "fearless", "dynamic", "determined", "diligent", "courteous", \
              "reasonable"
+
 nounsTwo   = "duck", "girl", "woman", "princess", "queen", "daisy", "invdividual", "intellect", "tulip", "koala bear", \
              "sex symbol", "specimen", "mermaid", "watermelon", "goose", "being", "goddess", "savant", "companion", \
              "baby-maker", "heart-throb", "citizen", "genius", "baller", "badass", "marsupial", "unicorn", "hero", "saint", \
