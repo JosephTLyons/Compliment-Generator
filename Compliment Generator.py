bc8a7317f8cb10b0d6389f3ccab1c2b79fd0d7b0
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2017-08-27 09:13:41 -0400

    Resized again

--- Compliment Generator.py
+++ Compliment Generator.py
@@ -7,7 +7,7 @@ from Tkinter import *
 master = Tk()
 master.title ("Compliment Generator")
 master.resizable (width = False, height = False)
-width  = 420
+width  = 450
 height = 105
 master.minsize (width, height)
 master.maxsize (width, height)
