a37473871391162310642387f40597f3c292c786
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2017-08-28 17:34:35 -0400

    removed unneeded comma

--- Words.py
+++ Words.py
@@ -10,7 +10,7 @@ adjectives = "sexy", "beautiful", "astonishing", "fabulous", "extraordinary", "g
              "resourceful", "swag-tastic", "cooperative", "exuberant", "productive", "stellar", "divine", "adventurous", \
              "fearless", "gregarious", "rational" , "sincere" , "witty", "versatile", "sociable", "pro-active", "practical", \
              "optimistic", "loyal", "imaginative", "humorous", "fearless", "dynamic", "determined", "diligent", "courteous", \
-             "reasonable",
+             "reasonable"
 
 nounsTwo   = "duck", "girl", "woman", "princess", "queen", "daisy", "invdividual", "intellect", "tulip", "koala bear", \
              "sex symbol", "specimen", "mermaid", "watermelon", "goose", "being", "goddess", "savant", "companion", \
